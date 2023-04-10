ce2a291ab1fea042c86146ab805a83518aa6942f
RagaRanjini <ragaranjini2001@gmail.com>
2023-04-10 17:31:30 +0530

    commit

--- assignment3-april5/Timer/Timer.js
+++ assignment3-april5/Timer/Timer.js
@@ -2,13 +2,14 @@ var TodayDate=new Date();
 var hours= 0;
 var minutes= 0;
 var seconds= 0;
-var run=0;
-var pause=0;
+var running=false;
+var intervalID=null;
+let timeref
 // console.log(hours+":"+minutes+":"+seconds);
 function startTimer(){
-    run++;
-    if(run==1){
-        setInterval(function(){
+    if(!running){
+        running=true;
+        timeref = setInterval(function(){
             document.getElementById("timer").innerHTML=String(hours).padStart(2, '0')+":"+String(minutes).padStart(2, '0')+":"+String(seconds).padStart(2, '0');
             if(seconds==60){
                 minutes++;
@@ -25,10 +26,13 @@ function startTimer(){
     }
 }
 function stopTimer(){
-    current_hour=hours;
-    current_minute=minutes;
-    current_second=seconds;
-    document.getElementById("notify").innerHTML="Timer stopped at "+current_hour+":"+current_minute+":"+current_second;
-    alert("Timer stopped at "+current_hour+":"+current_minute+":"+current_second);
+    if(running){
+        running=false;
+        clearInterval(timeref)
+    }else{
+        alert("Timer is already stopped !");
+    }
+}
+function resetTimer(){
     location.reload();
     }
