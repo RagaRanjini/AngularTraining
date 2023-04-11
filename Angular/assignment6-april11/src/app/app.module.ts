91c8163ca671b0bc93f6b4daa7a1b8c7ff436796
RagaRanjini <ragaranjini2001@gmail.com>
2023-04-11 16:44:09 +0530

    commit changes

--- Angular/assignment6-april11/src/app/app.module.ts
+++ Angular/assignment6-april11/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { ServiceBoxComponent } from './service-box/service-box.component';
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
+import { FooterBoxComponent } from './footer-box/footer-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
-    ServiceBoxComponent
+    HeaderMenuComponent,
+    SlideshowComponent,
+    FooterBoxComponent
   ],
   imports: [
     BrowserModule,
