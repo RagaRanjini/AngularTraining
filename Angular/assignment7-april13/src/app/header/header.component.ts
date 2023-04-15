ce66c29878a32b6a7c3dcbedf64858756764db9f
RagaRanjini <ragaranjini2001@gmail.com>
2023-04-15 12:03:41 +0530

    commit

---
+++ Angular/assignment7-april13/src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  menu:string[]=["opt1","opt2","opt3","opt4"]
+}
